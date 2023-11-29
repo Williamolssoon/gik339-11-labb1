70a884b3c3cb10614bf6275efb8b702fb4d7544f
Victorkajgard <113024813+Victorkajgard@users.noreply.github.com>
2023-11-29 14:14:52 +0100

    done

--- script.js
+++ script.js
@@ -1,46 +1,31 @@
 const checkbox = document.getElementById("divStyle");
-console.log(checkbox);
 
-const textfields = document.getElementsByClassName("textfield");
-console.log(textfields);
+const textfields = document.querySelectorAll(".textfield");
 
 const button = document.querySelector("#button");
-console.log(button);
 
 const divs = document.getElementById("divId");
-console.log(divs);
 
-
-
-
-//textfields.forEach((field) => field.addEventListener("blur", handleBlur));
-
-textfields[1].addEventListener("blur", handleBlur);
+textfields.forEach((field) => field.addEventListener("blur", handleBlur));
 
 checkbox.addEventListener("change", function(){
   divs.style.backgroundColor = textfields[0].value;
 });
 
-// button.addEventListener("click", handleClick);
+button.addEventListener("click", function(e){
+  e.preventDefault();  
+  divs.remove();
+});
 
 function handleBlur(e) {
-    console.log("handleBlur, eventobjekt:", e);
-    const name = e.target.name;
-    const value = e.target.value;
-  
+  const name = e.target.name;
+  const value = e.target.value;
     
-    divs.innerHTML = value;
-
+  console.log(name);
+  
+    if(name == "content"){
+      divs.innerHTML = value;
+    }
   }
-
-function handleClick(e) {
-  console.log("hej")
-  // const name = e.target.name;
-  // const value = textfields.value;
-
-  // if(name == "color"){
-  //   divs.style.backgroundColor = value;
-  // }
-}
 
   
